3f745937a943269f6078cbfd0c23daee762ea559
vinothM <vinodmad@34gmail.com>
2023-12-02 12:51:02 +0530

    Web--Mouse & keyboard event

--- src/Components/New Notes/FunctionalComp.tsx
+++ src/Components/New Notes/FunctionalComp.tsx
@@ -1,3 +1,5 @@
+import React, { useState } from "react"
+
 type functionalProps={
    person:{
     name:string
@@ -9,11 +11,22 @@ type functionalProps={
 }
 
 export const FunctionalComp:React.FunctionComponent<functionalProps>=(props)=>{
+    const [personName,setpersonName]=useState('vinoth')
     console.log(props)
+    const handleChange:React.MouseEventHandler<HTMLButtonElement>=()=>{
+        console.log("change")
+        setpersonName("rafiya")
+    }
+
+    const changeEvent=(e:React.KeyboardEvent<HTMLInputElement>)=>{
+        console.log("key press event occurs",e.keyCode)
+    }
     return(
         <>
         <h2 style={props.styling}>React Component typescript values {props.person.name} and age is {props.person.age}</h2>
         {props.children}
+        <button onClick={handleChange}>Onclick {personName}</button>
+        <input type="text" onKeyDown={(e)=>changeEvent(e)}/>
         </>
         )
 }
